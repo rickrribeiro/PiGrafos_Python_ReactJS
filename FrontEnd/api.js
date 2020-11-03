d4ffcc2b3a55fc660a97950fad459529d70f2a97
rickrribeiro <rickribeiro99@gmail.com>
2020-11-02 23:42:29 -0300

    get n edges

--- FrontEnd/api.js
+++ FrontEnd/api.js
@@ -144,13 +144,28 @@ const getNNodes = async () => {
   document.getElementById("nnodesText").innerHTML = myJson
 }
 
+const getNEdges = async () => {
+  const response = await fetch('http://127.0.0.1:5000/getNEdges', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': "GET, PUT, POST, DELETE, HEAD, OPTIONS"
+    }
+  });
+  
 
+  const myJson = await response.json(); //extract JSON from the http response
+  console.log(myJson)
+  // do something with myJson
+  document.getElementById("nedgesText").innerHTML = myJson
+}
 
 const calculus = async () =>{
 	//degreeText1
 	getDegree()
 	getNNodes()
-	//getEdge()
+	getNEdges()
 	//getComponent()
 	//getDensity()
 	getAdjList()
